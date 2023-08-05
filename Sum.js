8151115b68aeb944fe3bfb777712c70deb31ec7b
sandeep singh <sandeepsingh661987@gmail.com>
2023-08-05 22:39:05 +0530

    added message in Sun.js

---
+++ Sum.js
@@ -0,0 +1,11 @@
+
+const numbers = [2, 4, 6, 8, 10, 12, ,55, 78];
+
+const square = function(number){
+    return number * number;
+}
+
+const squareNumber = numbers.map(square);
+console.log(squareNumber);
+
+
